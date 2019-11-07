12f70c204d671f7900271f440fad95864f0f85d3
kenkensik <kenkensik@yahoo.co.jp>
2019-11-07 23:35:27 +0900

    Lesson13

---
+++ core/src/jp/techacademy/kento/saka/jumpactiongame/ActivityRequestHandler.kt
@@ -0,0 +1,5 @@
+package jp.techacademy.kento.saka.jumpactiongame
+
+interface ActivityRequestHandler {
+    fun showAds(show: Boolean)
+}
--- core/src/jp/techacademy/kento/saka/jumpactiongame/JumpActionGame.kt
+++ core/src/jp/techacademy/kento/saka/jumpactiongame/JumpActionGame.kt
@@ -8,7 +8,7 @@ import com.badlogic.gdx.graphics.g2d.SpriteBatch
 import com.badlogic.gdx.Game
 
 
-class JumpActionGame : Game() {
+class JumpActionGame(val mRequestHandler: ActivityRequestHandler) : Game() {
     lateinit var batch: SpriteBatch
 
     override fun create() {
